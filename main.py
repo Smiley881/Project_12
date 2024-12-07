133645adfe2ed5141ce28cffe3da8336af59c24b
Pablo_3_16 <ergle.pasha@gmail.com>
2024-12-07 13:59:04 +0300

    Реализована форма получения города

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests, json
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def main():
+    if request.method == 'GET':
+        return render_template('main.html')
+    else:
+        print(request.form['city_start'])
+        print(request.form['city_end'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
